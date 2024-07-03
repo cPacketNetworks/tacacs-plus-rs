a1a63acdfcc700681110a9f8dd4006452844c5b0
Zane Othman <git@formulaic.cloud>
2024-07-03 12:03:57 -0700

    move AsciiStr tests to doctests

--- src/ascii.rs
+++ src/ascii.rs
@@ -13,6 +13,18 @@ pub struct AsciiStr<'string>(&'string str);
 impl<'string> AsciiStr<'string> {
     /// Attempts to convert a `&str` to an `AsciiStr`.
     /// Returns `Some` if the string is indeed ASCII, and `None` otherwise.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use tacacs_plus::AsciiStr;
+    ///
+    /// let valid_ascii = "a string";
+    /// assert!(AsciiStr::try_from_str(valid_ascii).is_some());
+    ///
+    /// let beyond_ascii = "💀";
+    /// assert!(AsciiStr::try_from_str(beyond_ascii).is_none());
+    /// ```
     pub fn try_from_str(string: &'string str) -> Option<Self> {
         if string.is_ascii() {
             Some(Self(string))
@@ -23,6 +35,18 @@ impl<'string> AsciiStr<'string> {
 
     /// Attempts to convert a byte slice to an `AsciiStr`.
     /// Returns `Some` if the slice contains only ASCII codepoints, and `None` if not.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use tacacs_plus::AsciiStr;
+    ///
+    /// let valid_ascii = b"all ASCII characters with a\ttab";
+    /// assert!(AsciiStr::try_from_bytes(valid_ascii).is_some());
+    ///
+    /// let invalid_utf8 = [0x80]; // where'd the rest of the codepoint go?
+    /// assert!(AsciiStr::try_from_bytes(invalid_utf8.as_slice()).is_none());
+    /// ```
     pub fn try_from_bytes(bytes: &'string [u8]) -> Option<Self> {
         if bytes.is_ascii() {
             core::str::from_utf8(bytes).ok().map(Self)
--- src/ascii/tests.rs
+++
@@ -1,9 +0,0 @@
-use super::AsciiStr;
-
-#[test]
-fn invalid_ascii_string() {
-    assert!(
-        AsciiStr::try_from_str("💀").is_none(),
-        "AsciiStr with non-ASCII string should have failed"
-    );
-}
