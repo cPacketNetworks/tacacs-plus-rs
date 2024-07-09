386d5d1bfeda47d3eaaa4c21d8f938e736ade51b
Zane Othman <git@formulaic.cloud>
2024-07-09 09:43:26 -0700

    unconditionally mark no_std, and link to std as necessary

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,11 @@
 //!
 //! Rust library implementation of a TACACS+ ([RFC-8907](https://www.rfc-editor.org/rfc/rfc8907)) client.
 
-#![cfg_attr(not(feature = "std"), no_std)]
+#![no_std]
 #![warn(missing_docs)]
+
+#[cfg(feature = "std")]
+extern crate std;
 
 pub mod protocol;
 // mod session;
