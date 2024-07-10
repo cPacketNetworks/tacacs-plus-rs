adbb069ac9ca2ae98aeaf0fbf306bc536a4d9156
Zane Othman <git@formulaic.cloud>
2024-07-10 10:51:35 -0700

    mention TACACS+ over TLS in README/crate doc comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! # tacacs-plus
 //!
-//! Rust library implementation of a TACACS+ ([RFC-8907](https://www.rfc-editor.org/rfc/rfc8907)) client.
+//! Rust implementation of the TACACS+ ([RFC8907](https://www.rfc-editor.org/rfc/rfc8907)) and TACACS+ over TLS 1.3 ([IETF draft](https://datatracker.ietf.org/doc/draft-ietf-opsawg-tacacs-tls13/)) protocols.
 
 #![no_std]
 #![cfg_attr(feature = "docsrs", feature(doc_auto_cfg))]
