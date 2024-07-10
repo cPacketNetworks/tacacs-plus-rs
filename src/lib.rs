61b5e6b01faa012562b02b4e0a9fc4ce4d9b3b47
Zane Othman <git@formulaic.cloud>
2024-07-10 09:53:36 -0700

    add docsrs feature in preparation for publishing docs with std-exclusive API parts annotated as such

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 //! Rust library implementation of a TACACS+ ([RFC-8907](https://www.rfc-editor.org/rfc/rfc8907)) client.
 
 #![no_std]
+#![cfg_attr(feature = "docsrs", feature(doc_auto_cfg))]
 #![warn(missing_docs)]
 
 #[cfg(feature = "std")]
