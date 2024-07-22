eb0a0228f4390e419a5ba098f6d3db91f4fb5d99
Zane Othman <git@formulaic.cloud>
2024-07-22 09:09:57 -0700

    add test for xor_body_with_pad to ensure correct behavior

--- src/protocol/packet/tests.rs
+++ src/protocol/packet/tests.rs
@@ -1,6 +1,7 @@
 use super::*;
 
 use crate::protocol::accounting::Reply;
+use crate::protocol::{MajorVersion, MinorVersion, Version};
 
 #[test]
 fn obfuscated_packet_wrong_unencrypted_flag() {
@@ -54,5 +55,28 @@ fn unobfuscated_packet_wrong_unencrypted_flag() {
     assert_eq!(
         deserialize_error,
         DeserializeError::IncorrectUnencryptedFlag
+    );
+}
+
+#[test]
+fn obfuscate_correct_pad_generated() {
+    let header = HeaderInfo::new(
+        Version::new(MajorVersion::RFC8907, MinorVersion::V1),
+        7,
+        PacketFlags::empty(),
+        487514234,
+    );
+
+    // make buffer slightly over 1 MD5 output length, to also test truncation & MD5 iteration
+    let mut buffer = [0u8; 20];
+    xor_body_with_pad(&header, b"no one will guess this", &mut buffer);
+
+    assert_eq!(
+        buffer,
+        [
+            // known correct pad based on information in header
+            0x0d, 0x2e, 0xd1, 0x6f, 0xd6, 0x37, 0xab, 0x81, 0xc1, 0x3a, 0xc8, 0xf9, 0x19, 0xb4,
+            0x65, 0x48, 0x06, 0xf6, 0x5b, 0x41
+        ]
     );
 }
