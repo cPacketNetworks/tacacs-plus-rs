322eeb6cf1a93981c76c0a3cb35054670364726a
Zane Othman <git@formulaic.cloud>
2024-07-23 09:45:59 -0700

    Make fields of ArgumentOwned pub, for consistency

--- src/protocol/arguments/owned.rs
+++ src/protocol/arguments/owned.rs
@@ -7,30 +7,13 @@ use super::Argument;
 #[derive(Debug, PartialEq, Eq)]
 pub struct ArgumentOwned {
     /// The name of the argument.
-    pub(in crate::protocol) name: String,
+    pub name: String,
 
     /// The value of the argument.
-    pub(in crate::protocol) value: String,
+    pub value: String,
 
     /// Whether this argument is required.
-    pub(in crate::protocol) required: bool,
-}
-
-impl ArgumentOwned {
-    /// Gets the name of this argument.
-    pub fn name(&self) -> &str {
-        &self.name
-    }
-
-    /// Gets the value of this argument
-    pub fn value(&self) -> &str {
-        &self.value
-    }
-
-    /// Returns `true` if this argument is required, and `false` if it's optional.
-    pub fn is_required(&self) -> bool {
-        self.required
-    }
+    pub required: bool,
 }
 
 impl Argument<'_> {
