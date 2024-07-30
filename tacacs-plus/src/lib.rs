4acf4a93487973d928fd646fc78837a3024120bd
Zane Othman <git@formulaic.cloud>
2024-07-30 11:28:47 -0700

    add docsrs feature for tacacs-plus crate

--- tacacs-plus/src/lib.rs
+++ tacacs-plus/src/lib.rs
@@ -2,7 +2,7 @@
 //!
 //! Rust implementation of the TACACS+ ([RFC8907](https://www.rfc-editor.org/rfc/rfc8907)) protocol.
 
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(feature = "docsrs", feature(doc_auto_cfg))]
 #![warn(missing_docs)]
 #![warn(clippy::cast_lossless)]
 #![warn(clippy::cast_possible_truncation)]
