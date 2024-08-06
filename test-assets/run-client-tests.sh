6c7dc609e0e43df41bdddf266f742ac7a5c7facc
Zane Othman <git@formulaic.cloud>
2024-08-06 13:32:59 -0700

    conditionally build test server image if running in CI or not in test script

--- test-assets/run-client-tests.sh
+++ test-assets/run-client-tests.sh
@@ -3,14 +3,21 @@ set -euo pipefail
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
-# build server image
-docker build --tag tacacs-test-server --file "${REPO_ROOT}/test-assets/Dockerfile.test_server" "${REPO_ROOT}/test-assets"
+# if this script is running in CI, the image will already have been built
+if [ ! -v CI ]; then
+    # build server image
+    echo "Building test server Docker image..."
+    docker build --tag localhost/tacacs-test-server --file "${REPO_ROOT}/test-assets/Dockerfile.test_server" "${REPO_ROOT}/test-assets" >/dev/null
+    echo "Build finished!"
+fi
 
 # run server container in background
-docker run --rm --detach --publish 5555:5555 --name tacacs-server tacacs-test-server
+echo "Running server container in background"
+docker run --rm --detach --publish 5555:5555 --name tacacs-server localhost/tacacs-test-server >/dev/null
 
 # stop container on exit, including if/when a test fails
-trap "docker stop tacacs-server" EXIT
+trap "echo 'Stopping server container'; docker stop tacacs-server >/dev/null" EXIT
 
 # run all integration tests against server
+echo "Running tests..."
 cargo test --package tacacs-plus --test '*' --no-fail-fast
