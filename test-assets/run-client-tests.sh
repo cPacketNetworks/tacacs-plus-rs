dac5784d7db60634ddee4faa39947b261b0f6b1f
Zane Othman <git@formulaic.cloud>
2024-08-06 09:15:17 -0700

    move all build logic into run-client-tests.sh

--- test-assets/run-client-tests.sh
+++ test-assets/run-client-tests.sh
@@ -3,15 +3,24 @@ set -euo pipefail
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
-# build image
-docker buildx build --tag tacacs-test-server --file Dockerfile.test_server "${REPO_ROOT}/test-assets"
+# build server image & cache to GitHub Actions cache in CI
+# (the CI environment variable is always set while a workflow is running)
+if [ -v CI ]; then
+    docker buildx build --tag tacacs-test-server \
+        # cache both Dockerfile stages, not just final one
+        --cache-to type=gha,mode=max \
+        --cache-from type=gha,mode=max \
+        --file Dockerfile.test_server "${REPO_ROOT}/test-assets"
+else
+    # just build image normally if running outside of CI
+    docker buildx build --tag tacacs-test-server --file Dockerfile.test_server "${REPO_ROOT}/test-assets"
+fi
 
-# run container
+# run server container in background
 docker run --rm --detach --publish 5555:5555 --name tacacs-server tacacs-test-server
 
-# run tests against server
-# syntax from https://github.com/rust-lang/cargo/issues/8396#issuecomment-713126649
-cargo test --package tacacs-plus --test '*' --verbose
+# stop container on exit, including when a test fails
+trap "docker stop tacacs-server" EXIT
 
-# stop container
-docker stop tacacs-server
+# run all integration tests against server
+cargo test --package tacacs-plus --test '*' --no-fail-fast
