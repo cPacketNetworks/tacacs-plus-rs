4d93b8c80c35c51ae5b41f6a7db7d861434c3971
Zane Othman <git@formulaic.cloud>
2024-08-06 13:17:25 -0700

    simplify test script by moving cached build to dedicated docker actions

--- test-assets/run-client-tests.sh
+++ test-assets/run-client-tests.sh
@@ -3,25 +3,8 @@ set -euo pipefail
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
-# build server image & cache to GitHub Actions cache in CI
-# (the CI environment variable is always set while a workflow is running)
-if [ -v CI ]; then
-    # create docker-container driver builder, since default docker driver doesn't support GitHub Actions cache export
-    docker buildx create --name container --driver docker-container
-
-    # delete builder on exit
-    trap "docker buildx rm container" EXIT
-
-    # mode=max caches both Dockerfile stages, not just final one
-    docker buildx build --load --builder container \
-        --tag tacacs-test-server \
-        --cache-to type=gha,mode=max \
-        --cache-from type=gha,mode=max \
-        --file "${REPO_ROOT}/test-assets/Dockerfile.test_server" "${REPO_ROOT}/test-assets"
-else
-    # just build image normally (without caching) if running outside of CI
-    docker buildx build --tag tacacs-test-server --file "${REPO_ROOT}/test-assets/Dockerfile.test_server" "${REPO_ROOT}/test-assets"
-fi
+# build server image
+docker build --tag tacacs-test-server --file "${REPO_ROOT}/test-assets/Dockerfile.test_server" "${REPO_ROOT}/test-assets"
 
 # run server container in background
 docker run --rm --detach --publish 5555:5555 --name tacacs-server tacacs-test-server
