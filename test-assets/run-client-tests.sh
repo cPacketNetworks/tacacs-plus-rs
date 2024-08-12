639ac99de9106c719c68a0d7fa88eb55d6878838
Zane Othman <git@formulaic.cloud>
2024-08-12 16:39:28 -0700

    simplify TMPDIR logic by using already set TMPDIR variable, if applicable

--- test-assets/run-client-tests.sh
+++ test-assets/run-client-tests.sh
@@ -2,13 +2,7 @@
 set -euo pipefail
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
-
-# ensure temporary directory exists for tests
-if [ ! -v TMPDIR ]; then
-    TMPDIR=$(mktemp -d)
-    trap "rm -rf $TMPDIR" EXIT
-fi
-mkdir -p $TMPDIR
+TMPDIR=$(mktemp -d)
 
 if [ ! -v CI ]; then
     # build server image
@@ -17,20 +11,12 @@ if [ ! -v CI ]; then
     echo "Build finished!"
 fi
 
-# create accounting file
-touch $TMPDIR/accounting.log
-
 # run server container in background
 echo "Running server container in background"
-# TODO: revert after debugging
-# docker run --rm --detach --publish 5555:5555 --volume $TMPDIR/accounting.log:/tmp/accounting.log --name tacacs-server localhost/tacacs-test-server >/dev/null
-# debug flags: ACCT
-docker run --rm --detach --publish 5555:5555 --name tacacs-server localhost/tacacs-test-server -C /srv/tac_plus/tac_plus.conf -g -d 64 >/dev/null
+docker run --rm --detach --publish 5555:5555 --name tacacs-server localhost/tacacs-test-server >/dev/null
 
 # stop container on exit, including if/when a test fails
-# TODO: revert after debugging
-# trap "echo 'Stopping server container'; docker stop tacacs-server >/dev/null" EXIT
-trap "docker logs tacacs-server; echo 'Stopping server container'; docker stop tacacs-server >/dev/null" EXIT
+trap "echo 'Stopping server container'; docker stop tacacs-server >/dev/null" EXIT
 
 # run all integration tests against server
 echo "Running tests..."
