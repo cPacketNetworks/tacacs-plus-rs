82aca79b767da461b7b7a21f7ca6269295183ba1
Zane Othman <git@formulaic.cloud>
2024-08-13 11:32:39 -0700

    actually validate contents of accounting file

--- test-assets/run-client-tests.sh
+++ test-assets/run-client-tests.sh
@@ -7,7 +7,7 @@ TMPDIR=$(mktemp -d)
 if [ ! -v CI ]; then
     # build server image
     echo "Building test server Docker image..."
-    docker build --tag localhost/tacacs-test-server --file "${REPO_ROOT}/test-assets/Dockerfile.test_server" "${REPO_ROOT}/test-assets"
+    docker build --tag localhost/tacacs-test-server "${REPO_ROOT}/test-assets"
     echo "Build finished!"
 fi
 
@@ -22,7 +22,11 @@ trap "echo 'Stopping server container'; docker stop tacacs-server >/dev/null" EX
 echo "Running tests..."
 cargo test --package tacacs-plus --test '*' --no-fail-fast
 
-# copy accounting file out of container & verify contents
-# TODO: more specific validation?
+# copy accounting file out of container
 docker cp tacacs-server:/tmp/accounting.log $TMPDIR/accounting.log
-test $(wc -l $TMPDIR/accounting.log | awk '{print $1}') -eq 3
+
+# display contents of accounting file if verification fails
+trap "echo 'accounting file:'; cat $TMPDIR/accounting.log" ERR
+
+# verify contents of accounting file
+$REPO_ROOT/test-assets/validate_accounting_file.py $TMPDIR/accounting.log
